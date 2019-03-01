aa00e8f2182e949404262adf38515076275c8393
Victor Machado <victormachadogp@gmail.com>
2019-02-28 21:04:40 -0300

    Todas funcionalidades implementadas

--- js/app.js
+++ js/app.js
@@ -2,289 +2,238 @@
  * Create a list that holds all of your cards
  */
 
+//  shuffle dos cards
+$(document).ready(function () {
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
-     */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+    function discardCards() {
+        var listOfCards = ["fa-diamond", "fa-bomb", "fa-paper-plane-o", "fa-cube", "fa-leaf", "fa-bicycle", "fa-anchor", "fa-bolt", "fa-diamond", "fa-bomb", "fa-paper-plane-o", "fa-cube", "fa-leaf", "fa-bicycle", "fa-anchor", "fa-bolt"];
+
+        /*
+         * Display the cards on the page
+         *   - shuffle the list of cards using the provided "shuffle" method below
+         *   - loop through each card and create its HTML
+         *   - add each card's HTML to the page
+             */
+
+        shuffledListOfCards = shuffle(listOfCards);
+        console.log(shuffledListOfCards);
+
+
+        shuffledListOfCards.forEach(element => {
+            // elemnt = fa-diamond
+            var template = '<li class="card"><i class="fa ' + element + '"></i></li>'
+            $('#deck').append(template);
+
+        });
+
     }
 
-    return array;
-
-}
+    discardCards();
 
 
+    // Shuffle function from http://stackoverflow.com/a/2450976
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
 
-
-var guardarCard = null;
-
-
-
- $('.restart').click(function() {
-
-    $('.card').removeClass('open');
-    $('.card').removeClass('match');
-
-})
-
-// tenho que ver a classe do primeiro card e coletar e guardar
-// fazer a mesma coisa com o segundo card que eu clicar, só que a classe do segundo vai ser comparada com a do primeiro
-
-var guardarCard = null;
-var classe1Card;
-var segundoCard;
-
-var clicks = 0;
-    function hello() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
-        document.getElementById("cliques").innerHTML = clicks;
-        
-    };
-
-
-$('.card').click(function() {
-
-  
-    // tempo do meu jogo
-    // arrumar
-        // var minutesLabel = document.getElementById("minutes");
-        // var secondsLabel = document.getElementById("seconds");
-        // var totalSeconds = 0;
-        // setInterval(setTime, 1000);
-
-        // function setTime() {
-        // ++totalSeconds;
-        // secondsLabel.innerHTML = pad(totalSeconds % 60);
-        // minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-        // }
-
-        // function pad(val) {
-        // var valString = val + "";
-        // if (valString.length < 2) {
-        //     return "0" + valString;
-        // } else {
-        //     return valString;
-        // }}  
-
-
-    $(this).addClass('open');
-    console.log(guardarCard);
-
-
-    if (guardarCard === null) {
-        console.log("Primeiro Card Clicado");
-        classe1Card = $(this).children()[0].classList[1];
-        guardarCard = $(this);
-    }
-    // se o primeiro card não for nulo = 2 card
-    else if (guardarCard != null) {
-        console.log('esse é o segundo card');
-        var classe2Card = $(this).children()[0].classList[1];
-        segundoCard = $(this);
-
-        if (classe1Card === classe2Card) {
-            console.log('São iguais');
-            setTimeout(
-                function () {
-                    guardarCard.addClass('match');
-                    guardarCard = null;
-                }, 310);
-                setTimeout(
-                function () {
-                    segundoCard.addClass('match');
-               }, 310);
-            
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
         }
-        else {
-            console.log('são diferentes')
 
-            setTimeout(
-                function () {
-                    guardarCard.addClass('not-match');
-                    
-                    segundoCard.addClass('not-match');
-                }, 400);
+        return array;
 
-
-            setTimeout(
-                function () {
-
-                    guardarCard.removeClass('open');
-                    guardarCard.removeClass('not-match');
-                    guardarCard = null;
-                    segundoCard.removeClass('open');
-                    segundoCard.removeClass('not-match');
-                }, 1100);
-        }
-    }
-
-    
-        
-    
-
-    
-    // comparação do primeiro card com o segundo
-
-
-    else {
-        // se os card nao baterem, remover class open de ambos
-        $(guardarCard).removeClass('open');
-        $(segundoCard).removeClass('open');
     }
 
 
-    // checar se todos os card tem a classe open ao mesmo tempo 
-    if ($(".deck li.open").length == $(".deck li").length) {
-        console.log('agora terminou');
-        $('#myModal').modal('show');
 
-    } 
-      else {
-         console.log('ainda não terminou')
-     }
 
-     $('#reset').click(function() {
+
+
+    var guardarCard = null;
+
+
+    // botao restart
+    $('.restart').click(function deleteAll() {
+
         $('.card').removeClass('open');
         $('.card').removeClass('match');
-     } )
-
-
-
-
-
-
-
-
-    // if ($(classe2Card).children()[0].classList[2] === "match") {
-    //     console.log('jogo terminado');
-    // } else {
-
-    // }
-
-
-
-
-
-
-
-    // atribuir guardarCard para primeiro clique
-    // se uma card for clicado verificar se ele é ou não igual ao guardarCard
-
-
-
-
-})
-
-
-$('li').each(
-    function () {
-        var classes = this.classList;
-        for (var i = 0, len = classes.length; i < len; i++) {
-            if ($('li').hasClass(classes['open'])) {
-                $(this).addClass('Yeah');
-            }
+        $('.card').removeClass('not-match');
+        document.getElementById("clicks").innerHTML = "0";
+        clicks = 0;
+        $('.star1').removeClass('d-none');
+        $('.star2').removeClass('d-none');
+        $('#chronoExample .resetButton').show(function () {
+            timer.reset();
+        });
+        document.getElementById("deck").innerHTML = "";
+        discardCards();
+        $('.card').click(function () {
+            cardClicked(this)
         }
+        );
+
+
     })
 
 
+    var guardarCard = null;
+    var classe1Card;
+    var segundoCard;
+    var timer;
 
-// o que eu preciso fazer é um loop each entre todos os card da lista
-// os numeros que baterem que forem cards igual, vao ficar com a classe open nele mesmo
-// colocar o if pra quando forem igual os numeros da array, a classe open fica ativa
-// se os numeros da array forem diferentes, colocar o atributo else para tirar a classe open dos mesmos 
+    
+    // timer
+    timer = new easytimer.Timer();
+    timer.start();
+    timer.addEventListener('secondsUpdated', function (e) {
+        $('#basicUsage').html(timer.getTimeValues().toString());
+    });
 
-
-
-// os cards estão sendo validos para ver se são iguais antes da classe open ser chamada
-// isso quer dizer que o card que eu estou clicando por segundo tem que aparecer como open antes de verificar pra depois verficar
-// no caso é aquele esquema duration
-// a classe apareceria como open durante alguns segundos e logo depois a função continuria
-// apagando o open se os card tiverem classes diferetes e deixando se tiverem classes iguais
-
-// os card nao estao sendo comparados
-// isso porque o primeiro card perde seu valor apos o segundo clique
-
-// o card guardar card esta guardando apenas o primeiro valor que eu clico
-// vai ficar guardando pra sempre no null porque ele só fui null uma vez
-// isso porque null só igual ao card clicado no primeiro clique, depois disso nenhum card é null
-
-// uma função if separada que leia todas as terceira classes de todos os item dentro do li, assim como eu fiz la em cima using square bracktes
-// se essa função for verdadeira, então aparece um modal dizendo que o jogo foi ganho
+    var clicks = 0;
 
 
 
 
 
+    function cardClicked(self) {
 
-if (($('.anchor1') || $('.anchor2')).hasClass('open')) {
-    alert("right buddy");
-    console.log('cool buddy');
-} else {
+        console.log(self);
+        
 
-}
+        // contando o numero de cliques em cada card
+        clicks += 1;
+        document.getElementById("clicks").innerHTML = clicks;
+        document.getElementById("cliques").innerHTML = clicks;
 
-
-
-
-
-
-
-
-
+        if (clicks ==40) {
+            $('.star1').addClass('d-none');
+        } else if (clicks >= 50) {
+            $('.star2').addClass('d-none');
+        }
 
 
+
+        $(self).addClass('open');
+        console.log(guardarCard);
+
+        // verfica se o card é o primeiro card que se esta clicando
+        if (guardarCard === null) {
+            console.log("Primeiro Card Clicado");
+            classe1Card = $(self).children()[0].classList[1];
+            guardarCard = $(self);
+        }
+        // se o primeiro card não for nulo = segundo card
+        // segundo card abaixo:
+        else if (guardarCard != null) {
+            console.log('esse é o segundo card');
+            var classe2Card = $(self).children()[0].classList[1];
+            segundoCard = $(self);
+            // se o segundo card for igual o primeiro card
+            if (classe1Card === classe2Card) {
+                console.log('São iguais');
+                setTimeout(
+                    function () {
+                        guardarCard.addClass('match');
+                        guardarCard = null;
+                    }, 310);
+                setTimeout(
+                    function () {
+                        segundoCard.addClass('match');
+                    }, 310);
+
+            }
+            // se forem diferentes
+            else {
+                console.log('são diferentes')
+            // ficarao vermelhos e se moveram
+                setTimeout(
+                    function () {
+                        guardarCard.addClass('not-match');
+
+                        segundoCard.addClass('not-match');
+                    }, 400);
+
+                // apos ficarem vermelhos eles vao retornar virados para baixo
+                setTimeout(
+                    function () {
+
+                        guardarCard.removeClass('open');
+                        guardarCard.removeClass('not-match');
+                        guardarCard = null;
+                        segundoCard.removeClass('open');
+                        segundoCard.removeClass('not-match');
+                    }, 1100);
+            }
+
+
+
+        }
+
+
+
+
+        // checar se todos os card tem a classe open ao mesmo tempo 
+        if ($(".deck li.open").length == $(".deck li").length) {
+            console.log('agora terminou');
+            $('#finalTime').html(timer.getTimeValues().toString());
+            $('#myModal').modal('show');
+
+        }
+
+
+
+    }
+
+
+
+
+    $('.card').click(function () {
+        cardClicked(this)
+    }
+    );
+
+    // botao jogar novamente do Modal
+    $('#reset').click(function deleteAll() {
+
+
+        $('.card').removeClass('open');
+        $('.card').removeClass('match');
+        $('.card').removeClass('not-match');
+        document.getElementById("clicks").innerHTML = "0";
+        clicks = 0;
+        $('.star1').removeClass('d-none');
+        $('.star2').removeClass('d-none');
+        $('#chronoExample .resetButton').show(function () {
+            timer.reset();
+        });
+        document.getElementById("deck").innerHTML = "";
+        discardCards();
+        $('.card').click(function () {
+            cardClicked(this)
+        }
+        );
+
+        
+
+    })
+
+})
 
 
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - display the card's symbol (put self functionality in another function that you call from self one)
+ *  - add the card to a *list* of "open" cards (put self functionality in another function that you call from self one)
  *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ *    + if the cards do match, lock the cards in the open position (put self functionality in another function that you call from self one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put self functionality in another function that you call from self one)
+ *    + increment the move counter and display it on the page (put self functionality in another function that you call from self one)
+ *    + if all cards have matched, display a message with the final score (put self functionality in another function that you call from self one)
  */
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var guardarCard;
-
-// $('.card').click(function() {
-
-//     const segundoCard = $(this).children()[0].classList[1];
-
-//     guardarCard = $(this).children()[0].classList[1];
-//     $(this).addClass('open')
-//     console.log(guardarCard);
-
-//     if (guardarCard === segundoCard) {
-//         console.log("Yep Iguais");
-//     }
